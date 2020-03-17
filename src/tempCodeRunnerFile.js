8bee135a9e4c5d51134c6df2e85cf9bf4bfc1a01
antiyvt <antiy.vt@gmail.com>
2020-03-17 17:59:33 +0100

    Delete tempCodeRunnerFile.js

--- src/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const askName = () => {
-  console.log('Welcome to the Brain Games!');
-  const nameIs = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${nameIs}`);
-  return nameIs;
-};
